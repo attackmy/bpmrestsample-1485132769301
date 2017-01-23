c878bff7ab2c9f085a1dfcf8f41cca542cbcb47f
user1 <user1@example.com>
2017-01-23 10:22:50 +0900

    2.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var bodyParser = require('body-parser');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -15,6 +16,14 @@ var cfenv = require('cfenv');
 // create a new express server
 var app = express();
 
+app.get('/get', function (req, res) {
+  res.send({string01: 'string01!'});
+});
+app.post('/post', function (req, res) {
+  res.send(req.body);
+});
+app.use(bodyParser.json());
+
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
@@ -22,8 +31,7 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
+app.listen(appEnv.port, '0.0.0.0', function () {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
-  console.log("Hello everyone!");
 });
